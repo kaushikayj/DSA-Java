3565fedaa7f94e24d0ad0896a96d94f72824f47b
Kaushika YJ <kaushikayj@gmail.com>
2025-03-17 13:04:17 +0530

    updates

---
+++ Arrays/SortingProblems/SelectionSort.java
@@ -0,0 +1,45 @@
+package Arrays.SortingProblems;
+
+import java.util.Random;
+import java.util.Arrays;  // Import Arrays for printing
+
+public class SelectionSort {
+    public static void main(String args[]) {
+        int arr[] = new int[10];
+        Random random = new Random();
+
+        // Generating random values for the array
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+        }
+
+        // Print original array
+        System.out.println("Before sorting: " + Arrays.toString(arr));
+
+        // Sort the array
+        selectionSort(arr);
+
+        // Print sorted array
+        System.out.println("After sorting: " + Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int indexMin = i;
+
+            // Find the index of the smallest element in the remaining array
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[indexMin]) {
+                    indexMin = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element of the unsorted part
+            if (indexMin != i) { // Swap only if needed
+                int temp = arr[i];
+                arr[i] = arr[indexMin];
+                arr[indexMin] = temp;
+            }
+        }
+    }
+}
